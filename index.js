3287f1c92b068fe71c2e0ac238de9db01e81e6b9
Pierre Juarez <76144339+pierre-juarez@users.noreply.github.com>
2022-12-19 11:50:35 -0500

    [add] - Recursion simulator with POSTMAN

---
+++ index.js
@@ -0,0 +1,33 @@
+const newman = require('newman');
+const collection = require('./example.json');
+const async = require('async')
+
+
+const PARALLEL_RUN_COUNT = 30; // Peticiones en paralelo en 1 segundo
+
+const parametersForTestRun = {
+    collection: collection,
+    reporters: 'cli'
+};
+
+parallelCollectionRun = function (done) {
+    newman.run(parametersForTestRun, done);
+};
+
+let commands = []
+for (let index = 0; index < PARALLEL_RUN_COUNT; index++) {
+    commands.push(parallelCollectionRun);
+}
+
+// Runs the Postman sample collection thrice, in parallel.
+async.parallel(
+    commands,
+    (err, results) => {
+        err && console.error(err);
+
+        results.forEach(function (result) {
+            var failures = result.run.failures;
+            console.info(failures.length ? JSON.stringify(failures.failures, null, 2) :
+                `${result.collection.name} ran successfully.`);
+        });
+    });
